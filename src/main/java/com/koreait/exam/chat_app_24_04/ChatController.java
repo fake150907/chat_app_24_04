63ec8eac38c1f1fe8655206eccad13432bb57628
fake150907 <yungayeon223@gmail.com>
2024-04-11 15:52:08 +0900

    Post /chat/writeMessage , chatMessage 클래스 생성

---
+++ src/main/java/com/koreait/exam/chat_app_24_04/ChatController.java
@@ -0,0 +1,18 @@
+package com.koreait.exam.chat_app_24_04;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/chat")
+public class ChatController {
+    ChatMessage message = new ChatMessage("홍길동", "안녕하세요");
+
+    @PostMapping("/writeMessage")
+    @ResponseBody
+    public RsData writeMessage() {
+        return new RsData("S-1","메세지가 작성됨", message);
+    }
+}
